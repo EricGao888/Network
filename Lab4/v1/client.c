e2e827ef09331519a84b1d7a54b2e970517a3d97
Chufeng Gao <chufenggao@gmail.com>
2019-10-27 22:52:22 -0400

    Lab4 Add Network Security

--- Lab4/v1/client.c
+++ Lab4/v1/client.c
@@ -11,23 +11,22 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+int key_idx = 0;
+
+unsigned char mydecode(unsigned char x, unsigned int prikey);
+
 int main(int argc, char* argv[]) {
     int client_fd = 0, reader;
     struct sockaddr_in server_address;
     int server_address_len = sizeof(server_address);
-    char buffer[100];
+    unsigned char buffer[100];
+    unsigned int prikey;
+    char *ptr;
 
     memset(&server_address, 0, server_address_len);
 
-    // Parse the input from cmd
     if (argc == 1)
         printf("No Extra Command Line Argument Passed Other Than Program Name\n");
-    if (argc >= 2) {
-        for (int i = 3; i < argc; i++) {
-            strcat(buffer, argv[i]);
-            if (i != argc - 1) strcat(buffer, " ");
-        }
-    }
 
     if ((client_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         printf("\n Socket creation error \n");
@@ -39,9 +38,38 @@ int main(int argc, char* argv[]) {
     server_address.sin_port = htons(atoi(argv[2]));
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, argv[1], &server_address.sin_addr)<=0) {
+    if(inet_pton(AF_INET, argv[1], &server_address.sin_addr) <= 0) {
         printf("Invalid address/ Address not supported \n");
         return -1;
+    }
+
+    // Get private key from cmd line
+    prikey = strtoul(argv[3], &ptr, 10);
+//    printf("Key: %u\n", prikey);
+
+    for (int i = 3; i >= 0; i--) {
+        buffer[i] = (server_address.sin_addr.s_addr >> (8 * (3 - i))) & 255; // use the ip of server as request header to perform authentication
+    }
+
+    buffer[4] = '\0';
+
+    // Parse the input from cmd
+    int cur_len = 0;
+    if (argc >= 2) {
+        for (int i = 4; i < argc; i++) {
+            if (cur_len + strlen(argv[i]) > 30) {
+                printf("Illegal request dropped!\n");
+                exit(1);
+            }
+            strcat(buffer, argv[i]);
+            cur_len += strlen(argv[i]);
+            if (i != argc - 1) strcat(buffer, " ");
+        }
+    }
+
+    // Create certificate
+    for (; key_idx < strlen(buffer); key_idx++) {
+        buffer[key_idx] = mydecode(buffer[key_idx], prikey);
     }
 
     sendto(client_fd, (const char *)buffer, strlen(buffer),
@@ -51,4 +79,8 @@ int main(int argc, char* argv[]) {
     close(client_fd);
 
     return 0;
+}
+
+unsigned char mydecode(unsigned char x, unsigned int prikey) {
+    return x ^ (prikey >> ((key_idx++ % 4) * 8) ^ 255);
 }
--- Lab4/v1/server.c
+++ Lab4/v1/server.c
@@ -12,13 +12,21 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+int key_idx = 0;
+
+unsigned char myencode(unsigned char x, unsigned int pubkey);
+unsigned int parseIP(char line[]);
+unsigned int parseKey(char line[]);
+
 int main(int argc, char* argv[]) {
     int server_fd, new_fd, reader;
     struct sockaddr_in server_address, client_address;
     int server_address_len = sizeof(server_address), client_address_len = sizeof(client_address);
-    char buffer[100];
+    unsigned char buffer[100];
+    unsigned int pubkey;
     int status;
     int str_len;
+    int authentication;
     pid_t k;
 
     // Initialization, avoid undefined behavior
@@ -34,7 +42,7 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if (inet_pton(AF_INET, argv[1], &server_address.sin_addr)<=0) {
+    if (inet_pton(AF_INET, argv[1], &server_address.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
@@ -57,18 +65,65 @@ int main(int argc, char* argv[]) {
 
     // Read to process request
     while(1) {
+        key_idx = 0;
         int n;
-        n = recvfrom(server_fd, (char *)buffer, 100,
-                     MSG_WAITALL, ( struct sockaddr *) &client_address,
+        n = recvfrom(server_fd, (char *)buffer, sizeof(buffer),
+                     MSG_WAITALL, (struct sockaddr *) &client_address,
                      &client_address_len);
+        if (n > 34) {
+            printf("Illegal request dropped!\n");
+            continue;
+        }
         buffer[n] = '\0';
+
+        // Check ACL, retrieve public key
+        authentication = 0;
+        static const char filename[] = "acl.txt";
+        FILE *file = fopen(filename, "r" );
+        if (file != NULL) {
+            char line[100];
+            while (fgets(line, sizeof(line), file) != NULL) {
+                line[strlen(line) - 1] = '\0'; // Get rid of '\n'
+                unsigned int acl_ip = parseIP(line);
+                pubkey = parseKey(line);
+                if (acl_ip == client_address.sin_addr.s_addr) {
+                    authentication = 1;
+                    break;
+                }
+            }
+            fclose(file);
+        }
+        else {
+            perror(filename);
+        }
+
+        if (authentication == 0) {
+            printf("Client not in ACL, request dropped!\n");
+            continue;
+        }
+
+        // Decode request
+        for (; key_idx < strlen(buffer); key_idx++) {
+            buffer[key_idx] = myencode(buffer[key_idx], pubkey);
+        }
+
+        // Verify Certificate
+
+        for (int i = 0; i < 4; i++) {
+            if ((int)((server_address.sin_addr.s_addr >> (i * 8)) & 255) != (int)buffer[3 - i]) authentication = 0;
+        }
+        if (authentication == 0) {
+            printf("Client failed authentication check, request dropped!\n");
+            continue; // drop request which fails certificate check
+        }
+
         // print message from client
-//        printf("Message: %s\n", buffer);
+//        printf("Message: %s\n", buffer + 4);
 
         // Parse arguments
         char *arguments[strlen(buffer)];
         char *token;
-        token = strtok(buffer, " ");
+        token = strtok(buffer + 4, " ");
         int idx = 0;
         while (token != NULL) { 
             arguments[idx++] = token;
@@ -88,4 +143,38 @@ int main(int argc, char* argv[]) {
     	}
     }
     return 0;
+}
+
+unsigned char myencode(unsigned char x, unsigned int pubkey) {
+    return x ^ (pubkey >> ((key_idx++ % 4) * 8) ^ 255);
+}
+
+unsigned int parseIP(char line[]) {
+    char ip[100];
+    for (int i = 0; i < strlen(line); i++) {
+        if (line[i] != ' ') {
+            ip[i] = line[i];
+        }
+        else {
+            ip[i] = '\0';
+            break;
+        }
+    }
+    struct sockaddr_in tmp;
+    inet_pton(AF_INET, ip, &tmp.sin_addr);
+    return tmp.sin_addr.s_addr;
+}
+
+unsigned int parseKey(char line[]) {
+    char key[100];
+    char *ptr;
+    int i = 0;
+    for (; line[i] != ' '; i++);
+    i++;
+    int idx = 0;
+    for (; i < strlen(line); i++) {
+        key[idx++] = line[i];
+    }
+    key[idx] = '\0';
+    return strtoul(key, &ptr, 10);
 }
